808ae631c9bdb2e56c9e8104df50fe15180b9504
Zidan Abraham <zidangit07@gmail.com>
2023-01-18 16:35:32 +0800

    adding app & public

---
+++ app/core/App.php
@@ -0,0 +1,46 @@
+<?php
+class App {
+  protected $controller = 'Home';
+  protected $method = 'index';
+  protected $params = [];
+
+  public function __construct() {
+    $url = $this->parseURL();
+
+    // Controller
+    if (isset($url[0])) {
+      if (file_exists("../app/controllers/$url[0].php")) {
+        $this->controller = $url[0];
+        unset($url[0]);
+      }
+    }
+
+    require_once "../app/controllers/$this->controller.php";
+    $this->controller = new $this->controller;
+
+    // Method
+    if (isset($url[1])) {
+      if (method_exists($this->method, $url[1])) {
+        $this->method = $url[1];
+        unset($url[1]);
+      }
+    }
+
+    // Params
+    if (!empty($this->params)) {
+      $this->params = array_vallues($url);
+    }
+
+    // Run Controller, Method, and Params if any
+    call_user_func_array([$this->controller, $this->method], $this->params);
+  }
+  public function parseURL() {
+    if (isset($_GET['url'])) {
+      $url = $_GET['url'];
+      $url = rtrim($url, '/');
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+      $url = explode('/', $url);
+      return $url;
+    }
+  }
+}
---
+++ app/init.php
@@ -0,0 +1,2 @@
+<?php
+require_once 'core/App.php';

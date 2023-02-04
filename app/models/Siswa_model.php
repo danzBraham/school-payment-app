13c0f5ce3b2df59f4c17bced9a437b2568c00d03
Zidan Abraham <zidangit07@gmail.com>
2023-02-05 00:10:36 +0800

    fixing siswa model

--- app/models/Siswa_model.php
+++ app/models/Siswa_model.php
@@ -6,15 +6,15 @@ class Siswa_model {
     $this->db = new Database;
   }
 
+  public function getAllSiswa() {
+    $this->db->query("SELECT * FROM tb_siswa INNER JOIN tb_kelas USING(id_kelas)");
+    return $this->db->results();
+  }
+
   public function getSiswa($nis) {
     $this->db->query("SELECT * FROM tb_siswa INNER JOIN tb_kelas USING(id_kelas) WHERE nis = :nis");
     $this->db->bind('nis', $nis);
     return $this->db->result();
-  }
-
-  public function getAllSiswa() {
-    $this->db->query("SELECT * FROM tb_siswa INNER JOIN tb_kelas USING(id_kelas)");
-    return $this->db->results();
   }
 
   public function getAllKelas() {
@@ -24,21 +24,21 @@ class Siswa_model {
 
   public function insertSPP($nis, $thnAjaran) {
     $this->db->query("INSERT INTO tb_spp VALUES
-                    ('', ?, 'Juli', null, '?'),
-                    ('', ?, 'Agustus', null, '?'),
-                    ('', ?, 'September', null, '?'),
-                    ('', ?, 'Oktober', null, '?'),
-                    ('', ?, 'November', null, '?'),
-                    ('', ?, 'Desember', null, '?'),
-                    ('', ?, 'Januari', null, '?'),
-                    ('', ?, 'Februari', null, '?'),
-                    ('', ?, 'Maret', null, '?'),
-                    ('', ?, 'April', null, '?'),
-                    ('', ?, 'Mei', null, '?'),
-                    ('', ?, 'Juni', null, '?')");
+                    ('', :nis, 'Juli', null, ':thn_ajaran'),
+                    ('', :nis, 'Agustus', null, ':thn_ajaran'),
+                    ('', :nis, 'September', null, ':thn_ajaran'),
+                    ('', :nis, 'Oktober', null, ':thn_ajaran'),
+                    ('', :nis, 'November', null, ':thn_ajaran'),
+                    ('', :nis, 'Desember', null, ':thn_ajaran'),
+                    ('', :nis, 'Januari', null, ':thn_ajaran'),
+                    ('', :nis, 'Februari', null, ':thn_ajaran'),
+                    ('', :nis, 'Maret', null, ':thn_ajaran'),
+                    ('', :nis, 'April', null, ':thn_ajaran'),
+                    ('', :nis, 'Mei', null, ':thn_ajaran'),
+                    ('', :nis, 'Juni', null, ':thn_ajaran')");
 
     $this->db->bind('nis', $nis);
-    $this->db->bind('thnAjaran', $thnAjaran);
+    $this->db->bind('thn_ajaran', $thnAjaran);
     return $this->db->execute();
   }
 
@@ -59,9 +59,15 @@ class Siswa_model {
     $thnAjaranLusaDepan = implode('/', $thnAjaranLusaDepan);
 
     $this->db->query("INSERT INTO tb_siswa VALUES (
-      null, '$kelas', '$nama', '$password', '$alamat', '$telp'
+      null, :id_kelas, :nama, :password, :alamat, :no_telp
     )");
 
+    $this->db->bind('id_kelas', $kelas);
+    $this->db->bind('nama', $nama);
+    $this->db->bind('password', $password);
+    $this->db->bind('alamat', $alamat);
+    $this->db->bind('no_telp', $telp);
+    $this->db->execute();
     $nis = $this->db->lastID();
 
     $this->insertSPP($nis, $thnAjaran);
@@ -80,27 +86,29 @@ class Siswa_model {
     $alamat = $data['alamat'];
   
     $this->db->query("UPDATE tb_siswa SET
-                      id_kelas = :kelas,
+                      id_kelas = :id_kelas,
                       nama = :nama,
                       password = :password,
                       alamat = :alamat,
-                      no_telp = :telp
+                      no_telp = :no_telp
                       WHERE nis = :nis");
   
     $this->db->bind('id_kelas', $kelas);
     $this->db->bind('nama', $nama);
     $this->db->bind('password', $password);
     $this->db->bind('alamat', $alamat);
-    $this->db->bind('telp', $telp);
+    $this->db->bind('no_telp', $telp);
     $this->db->bind('nis', $nis);
     $this->db->execute();
     return $this->db->rowCount();
   }  
 
   public function deleteSiswa($nis) {
-    $this->db->query("DELETE FROM tb_transaksi WHERE nis = $nis");
-    $this->db->query("DELETE FROM tb_spp WHERE nis = $nis");
-    $this->db->query("DELETE FROM tb_siswa WHERE nis = $nis");
+    $this->db->query("DELETE FROM tb_transaksi WHERE nis = :nis");
+    $this->db->query("DELETE FROM tb_spp WHERE nis = :nis");
+    $this->db->query("DELETE FROM tb_siswa WHERE nis = :nis");
+    $this->db->bind('nis', $nis);
+    $this->db->execute();
     return $this->db->rowCount();
   }
 }
